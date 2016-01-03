47cf0d548758d95023115920a470b5a7b149bb79
Justin Mound <justinmound@gmail.com>
2016-01-03 13:32:55 -0500

    reconfiguring actions

---
+++ actions/lib/ansible-playbook.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+PLAYBOOK=$1
+STACK=$2
+LAYER=$3
+
+#ansible-playbook --connection=local -i /opt/omnia/plugins/inventory $PLAYBOOK -e stack=$STACK -e layer=$LAYER
+
+if [ -z $STACK ] ||  [ -z $LAYER ]; then
+  ansible-playbook --connection=local -i /opt/omnia/plugins/inventory $PLAYBOOK
+else
+  ansible-playbook --connection=local -i /opt/omnia/plugins/inventory $PLAYBOOK -e stack=$STACK -e layer=$LAYER
+fi
