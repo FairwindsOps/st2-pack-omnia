5faeb04d9c9c59cbce7bdd6c156c00a3cc83c809
Justin Mound <justinmound@gmail.com>
2016-01-07 11:54:36 -0500

    Adding supprt for the nevironment extra var

---
+++ actions/lib/ansible-stack-layer-version-repo-environment.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+PLAYBOOK=$1
+STACK=$2
+LAYER=$3
+VERSION=$4
+APP_REPO=$5
+ENVIRONMENT=$6
+
+
+if [ -z $PLAYBOOK ] ||  [ -z $STACK ] ||  [ -z $LAYER ] ||  [ -z $VERSION ] ||  [ -z $APP_REPO ] ||  [ -z $ENVIRONMENT ]; then
+  exit 1
+fi
+ansible-playbook $PLAYBOOK -e stack=$STACK -e layer=$LAYER -e version=$VERSION -e app_repo=$APP_REPO -e env=$ENVIRONMENT | tr '{{' '-'
