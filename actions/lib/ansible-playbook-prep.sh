4160bfa9733ffcd0f09618010876c596d26b47b4
Sarah Zelechoski <sarah@reactiveops.com>
2016-01-08 15:40:00 -0500

    simplify playbook

--- actions/lib/ansible-playbook-prep.sh
+++ actions/lib/ansible-playbook-prep.sh
@@ -3,8 +3,4 @@
 PLAYBOOK=$1
 BTYPE=$2
 
-if [ -z $TYPE ] ; then
-  ansible-playbook --connection=local -i /opt/omnia/infrastructure/plugins/inventory $PLAYBOOK
-else
-  ansible-playbook --connection=local -i /opt/omnia/infrastructure/plugins/inventory $PLAYBOOK -e app_base_type=$BTYPE 
-fi
+ansible-playbook --connection=local -i /opt/omnia/infrastructure/plugins/inventory $PLAYBOOK -e app_base_type=$BTYPE 
