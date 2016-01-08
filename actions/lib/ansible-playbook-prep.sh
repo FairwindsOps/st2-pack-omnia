d4b042c85e0c33da4708afd57ccc72511d8ba0bd
Sarah Zelechoski <sarah@reactiveops.com>
2016-01-08 15:35:10 -0500

    debug prep playbook

--- actions/lib/ansible-playbook-prep.sh
+++ actions/lib/ansible-playbook-prep.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 PLAYBOOK=$1
-TYPE=$2
+BTYPE=$2
 
 if [ -z $TYPE ] ; then
-  ansible-playbook --connection=local -i /opt/omnia/infrastructure/plugins/inventory $PLAYBOOK | tail -3 | head -1
+  ansible-playbook --connection=local -i /opt/omnia/infrastructure/plugins/inventory $PLAYBOOK
 else
-  ansible-playbook --connection=local -i /opt/omnia/infrastructure/plugins/inventory $PLAYBOOK -e app_base_type=$TYPE | tail -3 | head -1
+  ansible-playbook --connection=local -i /opt/omnia/infrastructure/plugins/inventory $PLAYBOOK -e app_base_type=$BTYPE 
 fi
