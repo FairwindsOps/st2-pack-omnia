4a6671c09a454c0bfe0546fdd2c003eb06e5d7c6
Sarah Zelechoski <sarah@reactiveops.com>
2016-01-25 17:00:00 -0500

    add shell scripts for each deploy step. create release-prod action

--- actions/lib/ansible-playbook-prep.sh
+++ actions/lib/ansible-playbook-prep.sh
@@ -5,4 +5,4 @@ ENGINE=$2
 ENGINE_MAJ_VERSION=$3
 ENGINE_VERSION=$4
 
-ansible-playbook --connection=local -i /opt/omnia/infrastructure/plugins/inventory $PLAYBOOK -e engine=$ENGINE -e engine_major_version=$ENGINE_MAJ_VERSION -e engine_version=$ENGINE_VERSION
+ansible-playbook --connection=local -i /opt/omnia/infrastructure/plugins/inventory $PLAYBOOK -e engine=$ENGINE -e engine_major_version=$ENGINE_MAJ_VERSION -e engine_version=$ENGINE_VERSION | tr '{{' '-'
