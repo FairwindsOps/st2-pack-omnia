e3e6495417bcfeea45352d349b257d5cef8c6c29
Sarah Zelechoski <sarah@reactiveops.com>
2016-01-26 12:14:49 -0500

    fix release action to use previous_env

--- actions/lib/ansible-playbook-release.sh
+++ actions/lib/ansible-playbook-release.sh
@@ -6,4 +6,4 @@ ENVIRONMENT=$3
 STACK=$4
 LAYER=$5
 
-ansible-playbook --connection=local -i /opt/omnia/infrastructure/plugins/inventory $PLAYBOOK -e env=$PREV_ENVIRONMENT -e env=$ENVIRONMENT -e stack=$STACK -e layer=$LAYER | tr '{{' '-'
+ansible-playbook --connection=local -i /opt/omnia/infrastructure/plugins/inventory $PLAYBOOK -e previous_env=$PREV_ENVIRONMENT -e env=$ENVIRONMENT -e stack=$STACK -e layer=$LAYER | tr '{{' '-'
