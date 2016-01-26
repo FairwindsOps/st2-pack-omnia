d5959a827e5d1899e71a529aea950e93f675cbfe
Sarah Zelechoski <sarah@reactiveops.com>
2016-01-26 09:27:11 -0500

    add pack-code-sha action to deploy code from a specific branch

--- actions/lib/ansible-playbook-pack-code.sh
+++ actions/lib/ansible-playbook-pack-code.sh
@@ -4,5 +4,10 @@ PLAYBOOK=$1
 ENVIRONMENT=$2
 STACK=$3
 LAYER=$4
+SHA=$5
 
-ansible-playbook --connection=local -i /opt/omnia/infrastructure/plugins/inventory $PLAYBOOK -e env=$ENVIRONMENT -e stack=$STACK -e layer=$LAYER | tr '{{' '-'
+if [ -z $SHA ]
+  ansible-playbook --connection=local -i /opt/omnia/infrastructure/plugins/inventory $PLAYBOOK -e env=$ENVIRONMENT -e stack=$STACK -e layer=$LAYER | tr '{{' '-'
+else
+  ansible-playbook --connection=local -i /opt/omnia/infrastructure/plugins/inventory $PLAYBOOK -e env=$ENVIRONMENT -e stack=$STACK -e layer=$LAYER -e version=$SHA | tr '{{' '-'
+fi
