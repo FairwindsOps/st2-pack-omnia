ac54ae126f5abd57b65e9e35641b0be6abec2ac1
Sarah Zelechoski <sarah@reactiveops.com>
2016-01-26 10:22:02 -0500

    fix typo in pack code bash

--- actions/lib/ansible-playbook-pack-code.sh
+++ actions/lib/ansible-playbook-pack-code.sh
@@ -6,7 +6,7 @@ STACK=$3
 LAYER=$4
 SHA=$5
 
-if [ -z $SHA ]
+if [ -z $SHA ]; then
   ansible-playbook --connection=local -i /opt/omnia/infrastructure/plugins/inventory $PLAYBOOK -e env=$ENVIRONMENT -e stack=$STACK -e layer=$LAYER | tr '{{' '-'
 else
   ansible-playbook --connection=local -i /opt/omnia/infrastructure/plugins/inventory $PLAYBOOK -e env=$ENVIRONMENT -e stack=$STACK -e layer=$LAYER -e version=$SHA | tr '{{' '-'
